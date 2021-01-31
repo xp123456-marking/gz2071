da1728fcdb896d0b8c9a001666baf25163715270
xmq <123abc@qq.com>
2021-01-31 10:28:54 +0800

    暂时性的费

--- git/src/com/gec/demotest/UserService.java
+++ git/src/com/gec/demotest/UserService.java
@@ -9,12 +9,7 @@ public interface UserService {
 		void setyou();
 		//张三做了新的改进
 		void setyouing();
-	void delete();
-	//张三做了新的改进
-	void setyou();
-	//张三做了新的改进
-	void setyouing();
-	//动物吃时
+	
 	void gettginging();
 
 }
