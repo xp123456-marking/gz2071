499b2bfb2048c70d75f20a913957d17b0532e841
xmq <123abc@qq.com>
2021-01-31 10:30:34 +0800

    按输入法

--- git/src/com/gec/demotest/UserService.java
+++ git/src/com/gec/demotest/UserService.java
@@ -3,13 +3,6 @@ package com.gec.demotest;
 public interface UserService {
 	//添加一个adduser的方法
 	void addUser();
-	//张三的再一次的改动
-		void delete();
-		//张三做了新的改进
-		void setyou();
-		//张三做了新的改进
-		void setyouing();
 	
-	void gettginging();
 
 }
